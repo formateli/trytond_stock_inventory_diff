8f05de2e11df3b622b321c6ab136f2dfcacfd35f
formateli <formateli@gmail.com>
2020-12-01 20:10:28 -0500

    Line list simple

--- inventory.py
+++ inventory.py
@@ -1,14 +1,11 @@
 # This file is part of trytond-stock_inventory_diff module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import PoolMeta
 from trytond.model import fields
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
 from trytond.pool import Pool
-
-__all__ = ['InventoryLine']
 
 
 class InventoryLine(metaclass=PoolMeta):
@@ -18,23 +15,30 @@ class InventoryLine(metaclass=PoolMeta):
         fields.Float(
             'Diff', digits=(16, Eval('unit_digits', 2)),
             depends=['unit_digits']),
-            'get_diff_quantity')
+            'on_change_with_diff_quantity')
 
     @fields.depends('product', 'quantity', 'expected_quantity')
-    def on_change_product(self):
-        super(InventoryLine, self).on_change_product()
-        self.diff_quantity = None
-        if self.product:
-            self.diff_quantity = self.get_diff_quantity()
-
-    @fields.depends('quantity', 'expected_quantity')
-    def on_change_quantity(self):
-        self.diff_quantity = None
-        if self.quantity is not None:
-            self.diff_quantity = self.get_diff_quantity()
-
-    def get_diff_quantity(self, name=None):
+    def on_change_with_diff_quantity(self, name=None):
         if self.expected_quantity is None or \
                 self.quantity is None:
             return
         return self.quantity - self.expected_quantity
+
+#    @fields.depends('product', 'quantity', 'expected_quantity')
+#    def on_change_product(self):
+#        super(InventoryLine, self).on_change_product()
+#        self.diff_quantity = None
+#        if self.product:
+#            self.diff_quantity = self.get_diff_quantity()
+
+#    @fields.depends('quantity', 'expected_quantity')
+#    def on_change_quantity(self):
+#        self.diff_quantity = None
+#        if self.quantity is not None:
+#            self.diff_quantity = self.get_diff_quantity()
+
+#    def get_diff_quantity(self, name=None):
+#        if self.expected_quantity is None or \
+#                self.quantity is None:
+#            return
+#        return self.quantity - self.expected_quantity
